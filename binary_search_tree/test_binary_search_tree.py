e309595a1c0ca1920d2892df9eff146484cc45b9
Kizmet <bryantpatton@gmail.com>
2019-12-19 16:12:51 -0800

    traversals

--- binary_search_tree/test_binary_search_tree.py
+++ binary_search_tree/test_binary_search_tree.py
@@ -81,14 +81,16 @@ class BinarySearchTreeTests(unittest.TestCase):
         sys.stdout = io.StringIO()
         self.bst.bft_print(self.bst)
         output = sys.stdout.getvalue()
-        self.assertTrue(output == "1\n8\n5\n3\n7\n2\n4\n6\n" or
-                        output == "1\n8\n5\n7\n3\n6\n4\n2\n")
+        self.assertTrue(
+            output == "1\n8\n5\n3\n7\n2\n4\n6\n" or output == "1\n8\n5\n7\n3\n6\n4\n2\n"
+        )
 
         sys.stdout = io.StringIO()
         self.bst.dft_print(self.bst)
         output = sys.stdout.getvalue()
-        self.assertTrue(output == "1\n8\n5\n7\n6\n3\n4\n2\n" or
-                        output == "1\n8\n5\n3\n2\n4\n7\n6\n")
+        self.assertTrue(
+            output == "1\n8\n5\n7\n6\n3\n4\n2\n" or output == "1\n8\n5\n3\n2\n4\n7\n6\n"
+        )
 
         sys.stdout = io.StringIO()
         self.bst.pre_order_dft(self.bst)
@@ -102,5 +104,6 @@ class BinarySearchTreeTests(unittest.TestCase):
 
         sys.stdout = stdout_  # Restore stdout
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
